186c52ff3bca5047a34f0d34d8fa8044779be953
hkedia321 <harshit.kedia2015@vit.ac.in>
2017-09-26 03:58:32 +0530

    show my message box

---
+++ src/scripts/main.js
@@ -0,0 +1,56 @@
+/* global $ */
+var messageFormElement = document.getElementById("messageForm");
+var inputMessageElement = document.getElementById("inputMessage");
+var messagesHistoryElement = document.getElementById("messagesHistory");
+
+messageFormElement.addEventListener("submit", function (event) {
+	event.preventDefault();
+	handleMessageInputSubmit();
+});
+
+function handleMessageInputSubmit(){
+	event.preventDefault();
+	var message=inputMessageElement.value;
+	message=message.trim();
+	inputMessageElement.value="";
+	if(message===""){
+		return 0;
+	}
+	else{
+		var currentTimeString=getCurrentTimeString();
+		createMyMessage(message,currentTimeString);
+	}
+
+}
+
+function getCurrentTimeString() {
+	var ampm="AM";
+	var currentDate=new Date();
+	var hours=currentDate.getHours();
+	var minutes=currentDate.getMinutes();
+	var time="";
+	if(hours>12){
+		ampm="PM";
+		hours-=12;
+	}
+	if(hours===12){
+		ampm="PM";
+	}
+	time=hours+":"+minutes+" "+ampm;
+	return time;
+}
+
+function createMyMessage(message,timeString){
+	$(
+		"<div class='message-container message-container-my'> \
+		<div class='message-box message-my'> \
+		<div class='message-text'>"+message+"</div> \
+		<div class='message-time'>"+timeString+"</div> \
+		</div> \
+	</div>"
+	).appendTo(messagesHistoryElement);
+	messagesHistoryElement.scrollTop = messagesHistoryElement.scrollHeight;
+}
+function createSusiMessage(){
+
+}
